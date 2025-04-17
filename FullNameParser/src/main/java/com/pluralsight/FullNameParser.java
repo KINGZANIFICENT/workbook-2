98745009efee00602a86fb8375b96342ed183b46
KINGZANIFICENT <chefangel.vargas23@gmail.com>
2025-04-17 15:23:29 -0400

    finished FullNameParser

--- FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
+++ FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Main {
+public class FullNameParser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
